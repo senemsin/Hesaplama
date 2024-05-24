9429b03fbfb7721ca5a4ec9636c81bf9c9aa4a03
senemsin <167536285+senemsin@users.noreply.github.com>
2024-05-24 13:54:26 +0300

    güncelleme

--- Hesaplama/Form1.Designer.cs
+++ Hesaplama/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.vkigiris = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
@@ -38,19 +39,21 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 25.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(265, 18);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(173, 31);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(287, 48);
             this.label1.TabIndex = 0;
             this.label1.Text = "HESAPLAMA";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // vkigiris
             // 
             this.vkigiris.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.vkigiris.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.vkigiris.Location = new System.Drawing.Point(31, 137);
+            this.vkigiris.Location = new System.Drawing.Point(234, 110);
             this.vkigiris.Name = "vkigiris";
-            this.vkigiris.Size = new System.Drawing.Size(169, 172);
+            this.vkigiris.Size = new System.Drawing.Size(169, 79);
             this.vkigiris.TabIndex = 1;
             this.vkigiris.Text = "Vücut Kitle \r\nİndeksi";
             this.vkigiris.UseVisualStyleBackColor = false;
@@ -60,9 +63,9 @@
             // 
             this.button2.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button2.Location = new System.Drawing.Point(305, 137);
+            this.button2.Location = new System.Drawing.Point(234, 208);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(169, 172);
+            this.button2.Size = new System.Drawing.Size(169, 79);
             this.button2.TabIndex = 2;
             this.button2.Text = "Alan Ve \r\nÇevre Hesaplama\r\n";
             this.button2.UseVisualStyleBackColor = false;
@@ -72,11 +75,11 @@
             // 
             this.button3.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button3.Location = new System.Drawing.Point(595, 137);
+            this.button3.Location = new System.Drawing.Point(234, 306);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(169, 172);
+            this.button3.Size = new System.Drawing.Size(169, 79);
             this.button3.TabIndex = 3;
-            this.button3.Text = "button3";
+            this.button3.Text = "Faiz Hesaplama";
             this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
@@ -84,25 +87,26 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(632, 433);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.vkigiris);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button vkigiris;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Hesaplama/Form1.cs
+++ Hesaplama/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -32,6 +33,19 @@ namespace Hesaplama
         private void button3_Click(object sender, EventArgs e)
         {
             Form4.ShowDialog();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.BackColor = System.Drawing.Color.Transparent;
+            
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+            
         }
     }
 }
